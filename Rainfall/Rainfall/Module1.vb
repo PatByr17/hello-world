59bc8966b332e5f8c5d43cb735133f871e3b7e2f
PatByr17 <40701997+PatByr17@users.noreply.github.com>
2018-07-12 14:37:50 +0100

    Add files via upload

---
+++ Rainfall/Rainfall/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim rainfall(12) As Integer
+        Dim total As Integer
+        For m As Integer = 1 To 12
+            Console.Write("Month " + m.ToString + ": ")
+            rainfall(m) = Console.ReadLine
+            total += rainfall(m)
+        Next
+        Dim min As Integer
+        Console.WriteLine("What is the minimum value?")
+        min = Console.ReadLine
+        For i As Integer = 1 To 12
+            If rainfall(i) < min Then
+                Console.WriteLine(i.ToString + ", ")
+            End If
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
